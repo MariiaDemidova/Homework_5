a9d02ce747d019e4d4fd11a9bcf702b4a96317d5
marve <marve@DESKTOP-BTMOBVL>
2021-12-13 00:52:18 +0300

    Добавьте файлы проекта.

---
+++ Homework_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int Sum = 0;
+            int i = 0;
+            Console.WriteLine("Введите 7 чисел массива");
+            for(; i < 7;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Sum += array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическиое массива - {0}", Sum/i);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целочисленное число");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            int i, j;
+            int x = 1;
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                        if (x == 1) 
+                    {
+                        array[i, j] = 2 % 2;
+                        --x;
+                    }
+                    else
+                    {
+                        array[i, j] = 2 / 2;
+                        ++x;
+                    }
+                    Console.Write("{0, 2}", array[i, j]);
+                }
+                Console.WriteLine();
+                }
+            Console.ReadKey();
+
+        }
+    }
+}
